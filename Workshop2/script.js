7d1be2a478320bc06b43e7af14908e5edcc24546
Hythei <146536863+Hythei@users.noreply.github.com>
2024-09-05 15:47:16 +0300

    up

---
+++ Workshop2/script.js
@@ -0,0 +1,8 @@
+document.write("<cite><strong><p>If I had nine hours to chop down a tree, I'd spend the first six sharpening my ax.</p></strong><br>-Abraham Lincoln</cite><br>")
+
+function repeatingText(){
+    
+    document.write("Text and Stuff") ;
+}
+
+repeatingText();
