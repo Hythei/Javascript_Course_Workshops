3add9bf4d0664c4d6aef4ef0087d1af93604b5af
HytHei <heikki.hyttinen16@gmail.com>
2024-10-18 21:58:10 +0300

    xml got hands

--- Workshop07/script.js
+++ Workshop07/script.js
@@ -14,3 +14,19 @@ function parseData(){
         document.querySelector("pre").append(long_sentence);
     }
 }
+
+//ex2
+let button2 = document.querySelector("#Fetch2");
+button.addEventListener("click", loadXMLFile);
+function loadXMLFile(){
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET","http://iceberg-cycle.codio.io/5:%20Asynchronous%20JavaScript%20(AJAX)/famous-quotes.xml", true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.querySelector("#quotes").innerHTML = xmlhttp.responseText;
+        }
+    }
+}
