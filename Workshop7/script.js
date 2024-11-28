3ac42d2c3d23ae8438dd6c4494cb1b2686fd160f
HytHei <heikki.hyttinen16@gmail.com>
2024-11-28 20:42:00 +0200

    improved documentation

--- Workshop7/script.js
+++ Workshop7/script.js
@@ -15,6 +15,7 @@ function parseData(){
     }
 }
 
+//Pukkaa crossorigin erroria molemmista annetuista linkeistä, mutta olettaen että ne toimisivat, alla olevien pitäis olle kelpo scriptejä.
 //ex2
 let button2 = document.querySelector("#Fetch2");
 button2.addEventListener("click", loadXMLFile);
@@ -30,4 +31,39 @@ function loadXMLFile(){
         }
     }
 }
-// I've encountered a lot of problems with this, so in case this isn't ready by the deadline, I'll just do it in the coming week. 
+
+
+// ex3
+let button3 = document.querySelector("#Fetch3");
+let table3 = document.querySelector("#tabledata");
+button3.addEventListener("click", loadXMLFile3);
+function loadXMLFile3(){
+    var xmlhttp = new XMLHttpRequest();
+    const quote_table = document.createElement('table');
+
+
+    xmlhttp.open("GET","http://iceberg-cycle.codio.io/5:%20Asynchronous%20JavaScript%20(AJAX)/famous-quotes.xml", true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            const xmlDOC = xmlhttp.responseXML;
+            const quotes = xmlDOC.getElementsByTagName("quote");
+            const tbody = quote_table.querySelector('tbody');
+
+            for (let i = 0; i < quotes.length; i++){
+                const quoteTEXT = quotes[i].getElementsByTagName("quotation")[0].textContent;
+                const authorTEXT = quotes[i].getElementsByTagName("author")[0].textContent;
+
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                <td>${quoteTEXT}</td>
+                <td>${authorTEXT}</td>
+                `;
+                tbody.appendChild(row);
+            }
+            quote_table.innerHTML = '';
+            quote_table.appendChild(tbody);
+        }
+    }
+}
